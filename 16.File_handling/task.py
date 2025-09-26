cc0044b79dc02e951b7fb930409736bd6a39bd52
marison77 <marison399@gmail.com>
2025-09-26 10:20:26 +0530

    commit

---
+++ 16.File_handling/task.py
@@ -0,0 +1,13 @@
+# open("16.File_handling/demo.txt",'x')
+   
+
+# a = open("16.File_handling/task.txt",'x')
+# print(a.write("Hello World"))
+
+
+
+a = open('tas.txt','a')
+a.write("Helloooooo")
+
+a.read()
+
